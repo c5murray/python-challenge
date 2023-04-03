95a1ac9a372319c4bb66f615fc031d6b35487701
c5murray <126737988+c5murray@users.noreply.github.com>
2023-04-02 22:49:16 -0400

    Update main.py

--- python-challenge/main.py
+++ python-challenge/main.py
@@ -1,1 +1,10 @@
+# Create a Python script that analyzes the records to calculate each of the following:
+# The total number of months included in the dataset
+# The net total amount of "Profit/Losses" per the entire period
+# The average of amount of "Profit/Losses" per the entire period
+# The greatest increases in profits(date and amount) over the entire period
+# The greatest decrease in cosses in losses (date adn amount) over the entire period
+# Print the analysis to the terminal and export a text file with the results
 
+
+# Import 
