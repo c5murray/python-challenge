2ead8368b3949977854b35e60ec41dd93169170b
c5murray <126737988+c5murray@users.noreply.github.com>
2023-04-09 21:50:11 -0400

    Update pybank.py

--- python-challenge/PyBank/pybank.py
+++ python-challenge/PyBank/pybank.py
@@ -18,7 +18,7 @@ with open(csvpath) as csvfile:
     minProfitLossMonth = ' '
     
 # Each row will append the local list to include the month, Profit/Loss, and the Total Profit/Loss
-  for row in csvreader:
+for row in csvreader:
     Month.append(row[0])
     profitLoss.append(float(row[1]))
     totalProfitLoss += float(row[1])
@@ -35,13 +35,3 @@ print("Total Revenue: " + '${:,.2f}'.format(averageProfitLossChange))
 print("Average Revenue Change: " + '${:,.2f}'.format(averageProfitLossChange))
 print("Greatest Increase in Profits: " +str(minProfitLossMonth) +
       " " + '${:,.2f}'.format(minProfitLossChange))
-
-# Provide output for pybank_results file
-output = "pybank_results.txt"
-    file.write("Financial Analysis\n")
-    file.write("-----------------\n")
-    file.write("Total Months: " + strg(len(Month))+"\n")
-    file.write("Total Revenue: " + '${:,.2f}'.format(totalProfitLoss)+"\n")
-    file.write("Average Revenue Change: " + '${:,.2f}'.format(averageProfitLossChange)+"\n")
-    file.write("Greatest Increase in Profits: " + str(maxProfitLossMonth) + " " + '${:,.2f}'.format(maxProfitLossChange)+"\n")
-    file.write("Greatest Decrease in Profits: " + str(minProfitLossMonth) + " " + '${:,.2f}'.format(minProfitLossChange)+"\n")
